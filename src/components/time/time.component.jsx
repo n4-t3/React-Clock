4394df8dd8a84addadfcb52d5f20ab93aae79ee4
n4-t3 <nathanmekuria@gmail.com>
2022-10-08 23:42:29 +0900

    code cleanup and added animation

--- src/components/time/time.component.jsx
+++ src/components/time/time.component.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react'
 import TimeCSS from './time.module.scss'
 
 const timeLength = {
-    "Hours": [0,23],
-    "Minutes": [0,59],
-    "Seconds": [0,59]
+    "Hours": [0, 23],
+    "Minutes": [0, 59],
+    "Seconds": [0, 59]
 }
 
 const Time = (props) => {
@@ -25,28 +25,17 @@ const Time = (props) => {
                 const seconds = currentTime.getSeconds()
                 setTime(seconds)
             }
-        }, 1000);
+        }, 1000)
         return () => clearInterval(interval);
     }, [props.type]);
 
-    const runCallback = (cb) => {
-        return cb();
-    };
-
     return (
         <div className={TimeCSS.time_wrapper}>
-            {
-                runCallback(() => {
-                    const row = [];
-                        time-1>=minLength ? row.push(<div className={TimeCSS.time} key={time-1} > {("0" + (time-1)).slice(-2)}</div>) : row.push(<div className={TimeCSS.time} key={maxLength} > {maxLength}</div>)
+            <div className={`${TimeCSS.time} ${TimeCSS.slide_before_out}`} key={time-2}  > {time - 2 >= minLength ? `${("0" + (time - 2)).slice(-2)}`: `${maxLength+(time-1)}`} </div> 
+            <div className={`${TimeCSS.time} ${TimeCSS.slide_out}`} key={time-1}  > {time - 1 >= minLength ? `${("0" + (time - 1)).slice(-2)}`: `${maxLength}`} </div> 
+            <div className={`${TimeCSS.time} ${TimeCSS.active} ${TimeCSS.slide}`} key={time} > {("0" + time).slice(-2)}</div>
+            <div className={`${TimeCSS.time} ${TimeCSS.slide_in}`} key={time + 1} > {time + 1 <= maxLength ? `${("0" + (time + 1)).slice(-2)}`: `${("0" + minLength).slice(-2)}` }</div>
 
-                        row.push(<div className={`${TimeCSS.time} ${TimeCSS.active}`} key={time} > {("0" + time).slice(-2)}</div>);
-
-                        time+1<=maxLength ? row.push(<div className={TimeCSS.time} key={time+1} > {("0" + (time+1)).slice(-2)}</div>) : row.push(<div className={TimeCSS.time} key={minLength} > {("0" + minLength).slice(-2)}</div>)
-                        
-                    return row;
-                })
-            }
         </div >
     )
 }
