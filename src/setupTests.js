ca108124b47044c7a3bc182b410adeb8ba71eae5
n4-t3 <nathanmekuria@gmail.com>
2023-09-10 18:58:19 +0900

    notified about teh __tests__ folder

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,5 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+//tests are written in the __tests__ folder
